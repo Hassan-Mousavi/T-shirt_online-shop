6ed3c251ce0777072922957effc94847638fac2e
Hassan-Mousavi <hassanmousavi496@gmail.com>
2025-11-28 08:12:51 +0330

    darkmode logo Co-authored-by:Hassan <hassanmousavi496@gmail.com>

--- public/script/app.js
+++ public/script/app.js
@@ -2,15 +2,19 @@
 const sun = document.querySelector("#sun");
 const moon = document.querySelector("#moon");
 const html = document.querySelector(".html");
+const lightLogo = document.querySelector(".light-logo");
+const darkLogo = document.querySelector(".dark-logo");
 sun.addEventListener("click", function () {
   html.classList.toggle("dark");
   sun.classList.add("hidden");
   moon.classList.remove("hidden");
+  darkLogo.classList.add("hidden");
+  lightLogo.classList.remove("hidden");
 });
 moon.addEventListener("click", function () {
   html.classList.toggle("dark");
   sun.classList.remove("hidden");
   moon.classList.add("hidden");
+  lightLogo.classList.add("hidden");
+  darkLogo.classList.remove("hidden");
 });
-// new acount
-
--- public/script/login.js
+++ public/script/login.js
@@ -2,10 +2,15 @@ const newAcount = document.querySelector(".new_acount");
 const loginContent = document.querySelector(".login_content");
 const loginImage = document.querySelector(".login_img");
 const signIn = document.querySelector(".sign_in");
-
+const goLogin = document.querySelector(".go_login");
 newAcount.addEventListener("click", function () {
   signIn.classList.remove("hidden");
   signIn.classList.add("-translate-x-full");
   loginContent.classList.add("hidden");
   loginImage.classList.add("translate-x-full");
+});
+goLogin.addEventListener("click", function () {
+  signIn.classList.add("hidden");
+  loginContent.classList.remove("hidden");
+  loginImage.classList.remove("translate-x-full");
 });
