d603375e0e7e795eacc6776878d1f677cf659b31
Hassan-Mousavi <hassanmousavi400@gmail.com>
2025-12-24 20:14:32 +0330

    changable picture of basket picture

--- public/script/app.js
+++ public/script/app.js
@@ -44,3 +44,29 @@ filterBtn.forEach((btns) => {
     chevron.classList.toggle("rotate-180");
   });
 });
+// basket page codes
+////////////////////////////////////////////////////////////////////////
+// change picture of the basket picture
+const chevronRight = document.querySelector(".chevron_right");
+const mainPic = document.querySelector(".main_pic");
+const pictures = [
+  "./public/images/card1.png",
+  "./public/images/card2.png",
+  "./public/images/card3.png",
+  "./public/images/card5.png",
+  "./public/images/card4.png",
+];
+let index = 0;
+function nextPic() {
+  index++;
+  if (index >= pictures.length) {
+    index = 0;
+  }
+  mainPic.src = pictures[index];
+}
+chevronRight.addEventListener("click", nextPic);
+// add to favoriates part
+const heartIcon = document.querySelector(".heart_icon");
+heartIcon.addEventListener("click",function(){
+  
+})
