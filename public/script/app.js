ec91095eb85ef91840fa215abf193ffbeccc60d9
Hassan-Mousavi <hassanmousavi400@gmail.com>
2026-01-27 14:38:51 +0330

    new update timer

--- public/script/app.js
+++ public/script/app.js
@@ -1,11 +1,5 @@
 // Dark mode
 const toggleThemeBtns = document.querySelectorAll(".toggle-theme");
-const subMenuBtn = document.querySelector(".sort-btn");
-const subMenuList = document.querySelector(".sub-menu-list");
-const menuMobileChevron = document.querySelector(".menu-mobile-chevron");
-const menuCloseX = document.querySelector(".mobile-menu-x");
-const menuOpen = document.querySelector(".bars-open");
-const mobileMenu = document.querySelector(".mobile-menu");
 // dark mode
 toggleThemeBtns.forEach((btns) => {
   btns.addEventListener("click", function () {
@@ -19,11 +13,17 @@ toggleThemeBtns.forEach((btns) => {
   });
 });
 // sub menu list
+const subMenuList = document.querySelector(".sub-menu-list");
+const subMenuBtn = document.querySelector(".sort-btn");
+const menuMobileChevron = document.querySelector(".menu-mobile-chevron");
 subMenuBtn.addEventListener("click", () => {
   subMenuList.classList.toggle("hidden");
   menuMobileChevron.classList.toggle("rotate-180");
 });
 // mobile menu
+const menuCloseX = document.querySelector(".mobile-menu-x");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuOpen = document.querySelector(".bars-open");
 menuCloseX.addEventListener("click", function () {
   mobileMenu.classList.remove("right-0");
   mobileMenu.classList.add("-right-64");
@@ -34,7 +34,6 @@ menuOpen.addEventListener("click", function () {
 });
 // ////////////////////////////////////////////////////////////////
 // number of products
-
 const counter = document.getElementById("cart_counter");
 const addToBasket = document.querySelectorAll(".add_to_basket");
 let count = 0;
@@ -44,3 +43,13 @@ addToBasket.forEach((btns) => {
     counter.textContent = count;
   });
 });
+// timer of Takhfifat products
+const timerDay = document.querySelector(".label_days");
+const timerHour = document.querySelector(".label_hours");
+const timerMinute = document.querySelector(".label_minutes");
+const timersecond = document.querySelector(".label_seconds");
+let time = 50;
+const timer = function () {
+  const now = new Date();
+  
+};
