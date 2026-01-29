e3e1f1aa0333b212bc0a0a8f1cd0496bb23ce62e
Hassan-Mousavi <hassanmousavi400@gmail.com>
2026-01-29 11:15:48 +0330

    new update timer

--- public/script/app.js
+++ public/script/app.js
@@ -44,12 +44,27 @@ addToBasket.forEach((btns) => {
   });
 });
 // timer of Takhfifat products
+
 const timerDay = document.querySelector(".label_days");
 const timerHour = document.querySelector(".label_hours");
 const timerMinute = document.querySelector(".label_minutes");
-const timersecond = document.querySelector(".label_seconds");
-let time = 50;
-const timer = function () {
-  const now = new Date();
-  
+const timerSecond = document.querySelector(".label_seconds");
+timerDay.textContent = `${0} روز`;
+timerHour.textContent = `${0} ساعت`;
+timerMinute.textContent = `${0} دقیقه`;
+timerSecond.textContent = `${0} ثانیه`;
+let time = 10;
+const tick = function () {
+  const day = Math.floor(time / 86400);
+  const hour = Math.trunc(Math.floor(time % 86400) / 3600);
+  const min = Math.trunc(Math.floor(time % 3600) / 60);
+  const sec = time % 60;
+  timerDay.textContent = `${day} روز`;
+  timerHour.textContent = `${hour} ساعت`;
+  timerMinute.textContent = `${min} دقیقه`;
+  timerSecond.textContent = `${sec} ثانیه`;
+  // time--;
+  time === 0 ? clearInterval(tick) : time--;
 };
+
+setInterval(tick, 1000);
